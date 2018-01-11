5a2121edaf12703ba7a05da84822bf80dad7387e
Lentaro <qq934884267@gmail.com>
2018-01-11 17:16:43 +0800

    使用compose组合函数

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
@@ -9,9 +9,13 @@ import registerServiceWorker from './registerServiceWorker';
 import { counter, addGUN, jianGUN, addGUNAsync } from './index.redux'
 
 //结合chrom中的redux tools插件使用
-const reduxDevtools = windew.devToolsExtension
 //创建store并结合中间件thunk
-const store = createStore(counter, applyMiddleware(thunk))
+const store = createStore(counter, 
+//使用compose对几个函数进行组合
+compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension?window.devToolsExtension():()=>{}
+))
 
 
 function render(){
